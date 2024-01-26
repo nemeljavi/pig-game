60acbf8956d92f9449751fb4e3b1bc95b5deab96
nemeljavi <nemeljavi70@gmail.com>
2024-01-26 13:23:07 +0100

    btns añadidos en scriptjs

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// seleccionar elementos del DOM
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+//const btnHold = document.querySelector('.btn--hold')
+const btnHold = document.getElementsByClass('btn--hold')[0]
 
+console.log(btNew, btnRoll, btnHold)
+
+
+btnRoll.addEventListener('click', function () => {
+  console.log('Roll dice')
+  })
+
+btnNew.addEventListener('click', function () => {
+  console.log('New game')
+  })
+
+btnHold.addEventListener('click', function () => {
+  console.log('Pasar turno')
+  })
