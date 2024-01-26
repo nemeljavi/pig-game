9be5623feb40b02239a8c7ac630f473924058c07
nemeljavi <nemeljavi70@gmail.com>
2024-01-26 14:22:26 +0100

    añadidos current score dado

--- script.js
+++ script.js
@@ -4,8 +4,28 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 const diceEl = document.querySelector('.dice')
+const score0El = document.querySelector('#score--0')
+const score1El = document.querySelector('#score--1')
+const current0El = document.querySelector('#current--0')
+const current1El = document.querySelector('#current--1')
 
 // const btnHold = document.getElementsByClassName('btn--hold')[0]
+
+let currentScore, scores, activePlayer
+
+const init = function () {
+  currentScore = 0
+  activePlayer = 0
+  scores = [0, 0]
+  // diceEl.classList.add('hidden')
+  diceEl.style.display = 'none'
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+}
+
+init()
 
 console.log(btnNew, btnRoll, btnHold)
 
@@ -14,12 +34,15 @@ btnRoll.addEventListener('click', () => {
   const dice = Math.trunc(Math.random() * 6) + 1
 
   // mostrar el dado
+  diceEl.style.display = 'block'
   diceEl.src = `dice-${dice}.png`
 
   if (dice === 1) {
     // pierdes turno
   } else {
     // sumar el dado al current score
+    currentScore += dice
+    current0El.textContent = currentScore
   }
 })
 
@@ -30,13 +53,3 @@ btnNew.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
   console.log('Pasar turno')
 })
-
-// TODO:
-// llamar a función init que haga lo siguiente:
-// el dado por defecto es invisible
-// score1 y score2 a 0
-// currentScore1 y currentScore2 a 0
-
-// TODO:
-// cuando se hace click en el botón roll:
-// el dado se pone visible
